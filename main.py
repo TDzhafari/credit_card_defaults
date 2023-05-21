95a6996f655baf3bd0952c5b437979fae8436631
TDzhafari <45837908+TDzhafari@users.noreply.github.com>
2023-05-21 12:28:17 -0400

    added: hyperparameter tunning for dtree

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import xlrd
+import matplotlib.pyplot as plt
 from pathlib import Path
 from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, recall_score, roc_auc_score
+from sklearn.metrics import accuracy_score, recall_score, roc_auc_score, f1_score
 
 
 def read_dataframe() -> pd.DataFrame:
@@ -33,7 +34,7 @@ def describe_data(df):
     print(df.info())
 
 
-def run_model(df, model_type):
+def run_model(df, model_type, tune):
 
     # subset with independent variables
     X = df.drop('default payment next month', axis=1)
@@ -55,29 +56,75 @@ def run_model(df, model_type):
         y_pred = log_reg.predict(x_test)
         # evaluate the accuracy of the model
         accuracy = accuracy_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
         print('Accuracy:', accuracy)
+        print('F1 score: ', f1)
 
     elif model_type == 'dtree':
-        dtree = tree.DecisionTreeClassifier()
+
+        # create an instance of a decision tree classifier model
+        dtree = tree.DecisionTreeClassifier(random_state=42)
+
+        # train it on the training subset
         dtree = dtree.fit(x_train, y_train)
+
+        # evaluate the performance on the test subset
         y_pred = dtree.predict(x_test)
+
+        # visualize the tree
+        tree.plot_tree(dtree, feature_names=x_train.columns,
+                       max_depth=2, filled=True)
+
+        # performance measures for tree with default parameters
         accuracy = accuracy_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
         print('Accuracy:', accuracy)
+        print('F1 score: ', f1)
+
+        if tune == True:
+
+            # for loop to perform hyperparameter tuning for tree depth
+            for max_d in range(1, 25):
+
+                # Create an instance of the tree
+                model = tree.DecisionTreeClassifier(
+                    max_depth=max_d, random_state=42,)
+
+                # train the model
+                model.fit(x_train, y_train)
+                # print out the performance measures. I'll do accuracy.
+                print('The Training Accuracy for max_depth {} is:'.format(
+                    max_d), model.score(x_train, y_train))
+                print('The Validation Accuracy for max_depth {} is:'.format(
+                    max_d), model.score(x_test, y_test))
+                print('')
+
+            model = tree.DecisionTreeClassifier(
+                max_depth=4, random_state=42)
+
+            model.fit(x_train, y_train)
+
+            # performance measures for tree with default parameters
+            accuracy = accuracy_score(y_test, y_pred)
+            f1 = f1_score(y_test, y_pred)
+            print('Accuracy:', accuracy)
+            print('F1 score: ', f1)
 
     elif model_type == 'random_forest':
         rfc = RandomForestClassifier(n_estimators=100, criterion='gini', )
         rfc = rfc.fit(x_train, y_train)
         y_pred = rfc.predict(x_test)
         accuracy = accuracy_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
         print('Accuracy:', accuracy)
+        print('F1 score: ', f1)
 
     elif model_type == 'xgboost':
         pass
-
 
         #   used for testing purposes
 if __name__ == '__main__':
 
     raw_df = read_dataframe()
     describe_data(raw_df)
-    run_model(raw_df, 'random_forest')
+    run_model(raw_df, 'dtree', True)
